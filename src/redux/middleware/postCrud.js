3860cd75852801e8ac3269c6400f1a3843e9b3da
Ayala-Cohen <ayalula2000@gmail.com>
2021-08-02 14:50:21 +0300

    set post middleware

--- src/redux/middleware/postCrud.js
+++ src/redux/middleware/postCrud.js
@@ -1,2 +1,11 @@
 import actions from '../action'
 
+export const setPosts = ({ dispatch, getState }) => next => action => {
+    if (action.type === "SET_POSTS") {
+        let url = ""
+        if(!getState().posts)
+            dispatch(actions.setPosts)
+    }
+    return next(action)
+}
+
