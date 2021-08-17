d8ee04207b4a550f6555763a16c639d01105f3fe
Ayala-Cohen <ayalula2000@gmail.com>
2021-08-17 19:17:59 +0300

    start add magazine component

---
+++ src/components/addMagazine/addMagazine.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function AddMagazine() {
+    return (
+        <>
+        <input type="radio" value="Title"/>
+        <input type="radio" value="Logo"/><br/>
+        <input type="color" value="rgb(0, 0, 0)"/>
+
+        </>
+    )
+}
+export default AddMagazine
